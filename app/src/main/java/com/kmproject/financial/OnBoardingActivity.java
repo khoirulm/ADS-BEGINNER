b41a7cd2eca1ee00b2d90a5524e0527c381aabb8
khoirul muttaqin <khoirulmuttaqin08@gmail.com>
2021-03-04 20:42:57 +0700

    first commit

---
+++ app/src/main/java/com/kmproject/financial/OnBoardingActivity.java
@@ -0,0 +1,14 @@
+package com.kmproject.financial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.onboarding_activity);
+    }
+}
